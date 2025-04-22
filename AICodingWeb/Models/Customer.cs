9b426053b6c4fa19275141871646e537066ea4b8
Bruce Dunwiddie <shriop@hotmail.com>
2025-04-22 14:12:45 -0500

    More model fixes.

--- AICodingWeb/Models/Customer.cs
+++ AICodingWeb/Models/Customer.cs
@@ -6,7 +6,7 @@ namespace AICodingWeb.Models
     public class Customer
     {
         public int CustomerID { get; set; }
-        public string CustomerName { get; set; }
+        public string? CustomerName { get; set; }
         public int BillToCustomerID { get; set; }
         public int CustomerCategoryID { get; set; }
         public int? BuyingGroupID { get; set; }
@@ -21,17 +21,17 @@ namespace AICodingWeb.Models
         public bool IsStatementSent { get; set; }
         public bool IsOnCreditHold { get; set; }
         public int PaymentDays { get; set; }
-        public string PhoneNumber { get; set; }
-        public string FaxNumber { get; set; }
-        public string DeliveryRun { get; set; }
-        public string RunPosition { get; set; }
-        public string WebsiteURL { get; set; }
-        public string DeliveryAddressLine1 { get; set; }
-        public string DeliveryAddressLine2 { get; set; }
-        public string DeliveryPostalCode { get; set; }
-        public string PostalAddressLine1 { get; set; }
-        public string PostalAddressLine2 { get; set; }
-        public string PostalPostalCode { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? FaxNumber { get; set; }
+        public string? DeliveryRun { get; set; }
+        public string? RunPosition { get; set; }
+        public string? WebsiteURL { get; set; }
+        public string? DeliveryAddressLine1 { get; set; }
+        public string? DeliveryAddressLine2 { get; set; }
+        public string? DeliveryPostalCode { get; set; }
+        public string? PostalAddressLine1 { get; set; }
+        public string? PostalAddressLine2 { get; set; }
+        public string? PostalPostalCode { get; set; }
         public int LastEditedBy { get; set; }
     }
 } 
